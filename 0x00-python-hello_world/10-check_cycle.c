4b4dcf2d9e875e961e813433768b87af2a463cd6
Sergio Zamudio <szamu5@hotmail.com>
2019-09-09 16:36:53 -0500

    lisnked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+int check_cycle(listint_t *list)
+{
+listint_t *copy1, *copy2;
+
+	copy1 = list;
+	if (list == NULL)
+		return (0);
+	if (list == (*list).next)
+		return (1);
+	while (copy1 != NULL)
+	{
+		copy1 = (*copy1).next;
+		copy2 = list;
+		while (copy1 && copy2 != copy1)
+		{
+			if (copy2 == NULL)
+				return (0);
+			if (copy2 == (*copy1).next)
+				return (1);
+			copy2 = (*copy2).next;
+		}
+	}
+	return (0);
+}
