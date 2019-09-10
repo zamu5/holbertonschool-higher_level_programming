17b792087b76f4540b59b03f34c85ba0a87fb632
Sergio Zamudio <szamu5@hotmail.com>
2019-09-10 13:38:51 -0500

    remove a print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for x in range(0, 10):
     for y in range(0, 10):
-        if (x < y and x != 8):
+        if (x < y):
             print(x, end="")
-            print(y, end=", ")
-        elif (x < y):
-            print(x, end="")
-            print(y)
+            if (x != 8):
+                print(y, end=", ")
+            else:
+                print(y)
