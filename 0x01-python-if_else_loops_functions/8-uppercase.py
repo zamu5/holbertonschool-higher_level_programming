a260d9cb3f378388001312eca90d6b0c1d495e99
Sergio Zamudio <szamu5@hotmail.com>
2019-09-10 14:52:33 -0500

    FIX: correct string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         i = ord(x)
         if i > 96 and i < 123:
             i = i - 32
-        print(chr(i), end="")
-    print("")
+        print("{}".format(chr(i)), end="")
+    print()
