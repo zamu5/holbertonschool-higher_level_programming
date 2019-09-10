9a087c416711767b1ef0b392ce6f2c992279c2d9
Sergio Zamudio <szamu5@hotmail.com>
2019-09-10 13:57:31 -0500

    FIX: use format in print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for x in range(0, 10):
     for y in range(0, 10):
-        if (x < y):
-            print(x, end="")
-            if (x != 8):
-                print(y, end=", ")
-            else:
-                print(y)
+        if x < y and x != 8:
+            print("{}{}".format(x,y), end=", ")
+        elif x < y:
+            print("{}{}".format(x,y))
