c864debdecd76e83f1e79535845f72a060054f43
Sergio Zamudio <szamu5@hotmail.com>
2019-09-10 14:04:28 -0500

    FIX: second loop start at 1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for x in range(0, 10):
     for y in range(1, 10):
-        if x < y and x != 8:
-            print("{}{}".format(x,y), end=", ")
-        elif x < y:
-            print("{}{}".format(x,y))
+        if x <y:
+            if x != 8:
+                print("{}{}".format(x,y), end=", ")
+            else:
+                print("{}{}".format(x,y))
