6dbdbd273a2533bba0157baa2ae4a80e0cc3e45c
Sergio Zamudio <szamu5@hotmail.com>
2019-09-10 18:59:14 -0500

    FEATURE: insert a node in a sort list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * insert_node - insert a new sort node
+ * @head: head of the list
+ * @number: number to add
+ * Return: the addres of the new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *c1, *c2, *new;
+
+	if (!head)
+		return (NULL);
+	c1 = *head;
+	while (c1)
+	{
+		c2 = (*c1).next;
+		if (c2 && number < (*c2).n)
+		{
+			new = malloc(sizeof(listint_t));
+			if (!new)
+				return (NULL);
+			(*new).n = number;
+			(*new).next = c2;
+			(*c1).next = new;
+			return (new);
+		}
+		c1 = (*c1).next;
+	}
+	return (NULL);
+}
