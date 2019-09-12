b5d1fe965dfcfa3ac8eb3f63a3456edaa8e4a5d2
Sergio Zamudio <szamu5@hotmail.com>
2019-09-12 15:34:23 -0500

    FIX: correc when is only a argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} arguments:".format(len(sys.argv) - 1))
+    print("{} {}:".format(len(sys.argv) - 1, "arguments"
+                          if len(sys.argv) != 2 else "argument"))
     for x in range(1, len(sys.argv)):
         print("{}: {}".format(x, sys.argv[x]))
