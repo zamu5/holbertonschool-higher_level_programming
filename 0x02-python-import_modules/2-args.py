6d9c0710ecdd82789752a18af046cd329b2b3f61
Sergio Zamudio <szamu5@hotmail.com>
2019-09-12 15:47:41 -0500

    FIX: arguments. when is 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} {}:".format(len(sys.argv) - 1, "arguments"
-                          if len(sys.argv) != 2 else "argument"))
+    print("{} {}".format(len(sys.argv) - 1, "argument:"
+                         if len(sys.argv) == 2 else "arguments."
+                         if len(sys.argv) == 1 else "arguments:"))
     for x in range(1, len(sys.argv)):
         print("{}: {}".format(x, sys.argv[x]))
