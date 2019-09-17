9e632c4fbb29c5713f94db27a1a58990ce315aa3
Sergio Zamudio <szamu5@hotmail.com>
2019-09-17 14:11:20 -0500

    Fix: use reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     isinstance(my_list, list)
-    for x in range(len(my_list), 0, -1):
+    my_list.reverse()
+    for x in my_list:
         print("{:d}".format(x))
