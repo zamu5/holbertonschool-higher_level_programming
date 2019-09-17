db1fbcdb88bba738e14aedba8cfeb74a8587d56b
Sergio Zamudio <szamu5@hotmail.com>
2019-09-17 15:40:41 -0500

    FIX: with loops

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
-        print()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print ("{:d} ".format(matrix[i][j]), end="")
+        print("")
