0aa0519a428d3e7a2ac42fc92b263f9fcb51eeea
Sergio Zamudio <szamu5@hotmail.com>
2019-09-17 16:28:31 -0500

    FEATURE: return the max of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if not len(my_list):
         return None
-    else:
-        maxi = 0
-        for i in my_list:
-            if i > maxi:
-                maxi = i
-        return maxi
+    maxi = 0
+    for i in my_list:
+        if i > maxi:
+            maxi = i
+    return maxi
