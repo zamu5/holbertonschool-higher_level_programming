b64e63d54d921439f27a974983af09580690a3fa
Sergio Zamudio <szamu5@hotmail.com>
2019-09-17 13:43:57 -0500

    FIX: when index > len

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,11 +2,9 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    i = 0
+    if idx > len(my_list):
+        return my_list
     for x in my_list:
-        if i == idx:
-            my_list[i] = element
+        if x == idx:
+            my_list[x] = element
             return my_list
-        if i > idx:
-            return my_list
-        i = i + 1
