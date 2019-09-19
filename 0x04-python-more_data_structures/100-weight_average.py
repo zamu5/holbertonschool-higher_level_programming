347d3372af6b51778277051a0d7d3c3bcbbfc61d
Sergio Zamudio <szamu5@hotmail.com>
2019-09-19 16:28:31 -0500

    FEATURE: advance

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if my_list is None or my_list == []:
         return 0
     return(sum([i*j for i, j in my_list]) / sum([j for i, j in my_list]))
