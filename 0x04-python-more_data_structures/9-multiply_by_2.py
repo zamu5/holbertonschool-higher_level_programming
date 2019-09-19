aea6328564b1bcf551b1c3cc96a7820dd55f53af
Sergio Zamudio <szamu5@hotmail.com>
2019-09-19 14:56:34 -0500

    FIX: pep8

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    copy =  dict([(y, x*2) for y, x in a_dictionary.items()])
-    return copy
+    return dict([(y, x*2) for y, x in a_dictionary.items()])
