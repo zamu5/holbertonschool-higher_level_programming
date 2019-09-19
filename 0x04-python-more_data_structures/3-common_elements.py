e61cd2afa252bd76391a5abf023e4a6c3218aaac
Sergio Zamudio <szamu5@hotmail.com>
2019-09-19 12:29:06 -0500

    FIX: add more than one element

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    new = []
     for i in set_1:
         if i not in set_2:
             pass
         else:
-            return i
+            new.append(i)
+    return new
