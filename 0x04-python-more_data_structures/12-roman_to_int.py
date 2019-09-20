0b825ddddf9a14798b0c8ae0f77ef0219273dc71
Sergio Zamudio <szamu5@hotmail.com>
2019-09-19 20:36:34 -0500

    FEATURE: roman numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
     num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     valor = 0
     for key in range(len(roman_string)):
