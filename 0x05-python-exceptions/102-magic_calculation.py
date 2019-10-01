d1ead900c2ce22fecbdb974f0bc96e0cc3fb1fdc
Sergio Zamudio <szamu5@hotmail.com>
2019-09-30 23:45:11 -0500

    FEATURE: assembler

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if a > i:
+                raise Exception("too far")
+            else:
+                result += (a**b)/i
+        except:
+            result = a + b
+            break
+    return result
