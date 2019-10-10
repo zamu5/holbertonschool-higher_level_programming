305928e19c8580565d04333a2948f2f1225b803c
Sergio Zamudio <szamu5@hotmail.com>
2019-10-09 20:53:19 -0500

    fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
     if not isinstance(matrix, list):
-        raise TypeError("""matrix must be a matrix (list of lists)\
+        raise TypeError("""matrix must be a matrix (list of lists) \
         of integers/floats""")
     if len(matrix) is 0 or not all(len(l) > 0 for l in matrix):
-        raise TypeError("""matrix must be a matrix (list of lists)\
-        of integers/floats""")
+        raise TypeError("""matrix must be a matrix (list of lists) \
+of integers/floats""")
     if not all(len(l) == len(matrix[0]) for l in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("""matrix must be a matrix (list of lists)\
-            of integers/floats""")
+            raise TypeError("""matrix must be a matrix (list of lists) \
+of integers/floats""")
         if not all(isinstance(element, (int, float)) for element in row):
-            raise TypeError("""matrix must be a matrix (list of lists)\
-            of integers/floats""")
+            raise TypeError("""matrix must be a matrix (list of lists) \
+of integers/floats""")
     if div is 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(div, (int, float)):
