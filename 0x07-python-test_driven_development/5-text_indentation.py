129dd2ccbcde237af172d1910b93e26a76ce94d6
Sergio Zamudio <szamu5@hotmail.com>
2019-10-09 21:47:57 -0500

    casa

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,9 +2,11 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for i in text:
-        if i is '.' or i is '?':
-            print("{}\n".format(i))
-            flag = 1
-        else:
-            print("{}".format(i), end="")
+    s = text[:]
+    for d in ".?:":
+        list_text = s.split(d)
+        s = ""
+        for i in list_text:
+            i = i.strip(" ")
+            s = i + d if s is "" else s + "\n\n" + i + d
+    print(s[:-3], end="")
