ee666a024aa2b606abdd5540e5175304f3aecee0
Sergio Zamudio <szamu5@hotmail.com>
2019-10-16 21:32:27 -0500

    FIX: end=''

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,4 +2,4 @@
 def read_file(filename=""):
     """Read a file"""
     with open(filename, 'r', encoding="utf-8") as f:
-        print("{}".format(f.read()))
+        print("{}".format(f.read()), end="")
