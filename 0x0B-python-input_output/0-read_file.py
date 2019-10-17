325a183925c9e64c17bb13e0df14129e49dffe51
Sergio Zamudio <szamu5@hotmail.com>
2019-10-16 21:30:26 -0500

    Fix: pep8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """Read a file"""
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print("{}".format(f.read()))
--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 import sys
 import os
+
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 if __name__ == "__main__":
 
