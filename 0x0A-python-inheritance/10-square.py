66f77c47eaa21793a39f79b0c5cb8be6f8ca460f
Sergio Zamudio <szamu5@hotmail.com>
2019-10-17 21:34:14 -0500

    FIX: pep8

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Square #1"""
     def __init__(self, size):
         self.integer_validator("size", size)
-        super().__init__(size,size)
+        super().__init__(size, size)
         self.__size = size
