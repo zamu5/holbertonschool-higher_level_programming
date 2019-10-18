8f2b2cb3783ee9f7351abf09c7acf25f55286d77
Sergio Zamudio <szamu5@hotmail.com>
2019-10-17 21:48:18 -0500

    FEATURE: square 2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """Square #2"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
