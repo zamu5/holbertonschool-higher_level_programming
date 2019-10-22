c440c6ce85bd87e6dfea97d23afe93c3bcf22a0a
Sergio Zamudio <szamu5@hotmail.com>
2019-10-22 14:55:50 -0500

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+import json
+import os
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None and list_dictionaries == "[]":
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        li = []
+        fi = "{}.json".format(cls.__name__)
+        if list_objs is not None:
+            for i in range(len(list_objs)):
+                li.append(list_objs[i].to_dictionary())
+        li = cls.to_json_string(li)
+        with open(fi, 'w') as f:
+            f.write(li)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            create = cls(5, 5)
+        elif cls.__name__ == "Square":
+            create = cls(5)
+        create.update(**dictionary)
+        return create
+
+    @classmethod
+    def load_from_file(cls):
+        fi = "{}.json".format(cls.__name__)
+        if os.path.exists(fi) is False:
+            return []
+        with open(fi, 'r') as f:
+            lis = f.read()
+        lic = cls.from_json_string(lis)
+        lii = []
+        for i in range(len(lic)):
+            lii.append(cls.create(**lic[i]))
+        return lii
