a93106f50d6716b5581d9b04438c53348ee80d59
Sergio Zamudio <szamu5@hotmail.com>
2019-10-22 16:18:33 -0500

    coments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,23 +12,28 @@ class Square(Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
+        """__init__ function"""
         self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """size function"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """size function"""
         self.width = value
         self.height = value
 
     def __str__(self):
+        """__str__ function"""
         return "[Rectangle] ({}) {}/{} - {}".format(self.id, self.x,
                                                     self.y, self.width)
 
     def update(self, *args, **kwargs):
+        """update function"""
         if args is not None and len(args) is not 0:
             list = ["id", "size", "x", "y"]
             for i in range(len(args)):
@@ -46,6 +51,7 @@ class Square(Rectangle):
                     setattr(self, key, value)
 
     def to_dictionary(self):
+        """to_dictionary function"""
         lis = ["id", "size", "x", "y"]
         val = [self.id, self.size, self.x, self.y]
         return dict(zip(lis, val))
