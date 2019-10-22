96d1e7fb3ea64d665b236878800f49fb91e4bdcf
Sergio Zamudio <szamu5@hotmail.com>
2019-10-22 15:51:06 -0500

    coments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,6 +12,8 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """init"""
+
         self.width = width
         self.height = height
         self.x = x
@@ -20,10 +22,14 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """width"""
+
         return self.__width
 
     @width.setter
     def width(self, value):
+        """with setter"""
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -32,10 +38,14 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """height"""
+
         return self.__height
 
     @height.setter
     def height(self, value):
+        """height setter"""
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -44,10 +54,14 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """x"""
+
         return self.__x
 
     @x.setter
     def x(self, value):
+        """x setter"""
+
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
@@ -56,10 +70,14 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """y"""
+
         return self.__y
 
     @y.setter
     def y(self, value):
+        """y setter"""
+
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
@@ -67,18 +85,26 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """area"""
+
         return self.__width * self.__height
 
     def display(self):
+        """display"""
+
         print("\n" * self.__y, end="")
         for i in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
+        """str"""
+
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
                                                        self.width, self.height)
 
     def update(self, *args, **kwargs):
+        """update"""
+
         if args is not None and len(args) is not 0:
             list = ["id", "width", "height", "x", "y"]
             for i in range(len(args)):
@@ -88,6 +114,8 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """to dictionary"""
+
         list = ["x", "y", "id", "height", "width"]
         val = [self.x, self.y, self.id, self.height, self.width]
         return dict(zip(list, val))
