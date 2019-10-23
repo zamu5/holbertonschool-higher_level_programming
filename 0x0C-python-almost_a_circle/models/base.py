3b900390069e7795abd9b6fa690ca0c63a9a0e02
Sergio Zamudio <szamu5@hotmail.com>
2019-10-22 19:44:49 -0500

    AD

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ this module have the base class
 
 import json
 import os
+import csv
 
 
 class Base:
@@ -89,3 +90,38 @@ class Base:
         for i in range(len(lic)):
             lii.append(cls.create(**lic[i]))
         return lii
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ doc """
+        filename = cls.__name__ + ".csv"
+        listDic = []
+        with open(filename, 'w') as f:
+            fd = csv.writer(f, delimiter=',')
+            if list_objs is None:
+                fd.writerow([])
+            else:
+                fd.writerow(list(list_objs[0].to_dictionary().keys()))
+                for instance in list_objs:
+                    fd.writerow(list(instance.to_dictionary().values()))
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ doc """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, 'r') as f:
+                fd = csv.reader(f, delimiter=',')
+                instances = []
+                sw = 0
+                for row in fd:
+                    if sw == 0:
+                        keys = row
+                        sw = 1
+                    else:
+                        row = map(lambda x: int(x), row[:])
+                        instances.append(cls.create(**dict(zip(keys, row))))
+                return instances
+        except:
+            raise
+            return []
