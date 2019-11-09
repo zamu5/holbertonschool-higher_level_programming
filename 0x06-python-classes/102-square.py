7d973787e769a74c8362d44478fceb1d6b3135e2
Sergio Zamudio <szamu5@hotmail.com>
2019-11-09 00:18:57 -0500

    [FEATURE]: special methods New file: 102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, other):
+        return self.__size == other.__size
+
+    def __lt__(self, other):
+        return self.__size < other.__size
+
+    def __le__(self, other):
+        return self.__size <= other.__size
+
+    def __ne__(self, other):
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
