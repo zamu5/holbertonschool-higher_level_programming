591a2c4b5b30bfe994f17c51e3546411608c49f3
Sergio Zamudio <szamu5@hotmail.com>
2019-11-26 18:12:21 -0500

    [FIX]: change the host Modified: 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- show privileges
 -- Query to show the privileges of a user.
-SHOW GRANTS FOR user_0d_1;
-SHOW GRANTS FOR user_0d_2;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
