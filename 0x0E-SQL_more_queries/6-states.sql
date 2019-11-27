f1f430901aa675d915932fc822981e16449019d5
Sergio Zamudio <szamu5@hotmail.com>
2019-11-26 23:20:25 -0500

    [FEATURE]: table autoincrement New file:6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates DB and table
+-- Query to create database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+id INT NOT NULL AUTO_INCREMENT unique primary key,
+name VARCHAR(256) NOT NULL);
