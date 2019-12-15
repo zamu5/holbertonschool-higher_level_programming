693ed257cf5e1904974b4d209b2b13098b7a1abd
Sergio Zamudio <szamu5@hotmail.com>
2019-12-15 16:37:29 -0500

    [FEATURE]: Class State that inherits from Base. New file: model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+"""
+Module that have the class State
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Have id and name
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
