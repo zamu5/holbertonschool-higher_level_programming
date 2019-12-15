47296267e51e5973bde9bf5484889f70fdb8ebcd
Sergio Zamudio <szamu5@hotmail.com>
2019-12-15 13:23:47 -0500

    [FEATURE]: List all the name that are equal to state name searched New file: 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of
+ hbtn_0e_0_usa where name matches the argument
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WHERE name LIKE BINARY "{:s}" \
+    ORDER BY states.id ASC""".format(sys.argv[4]))
+    rows = c.fetchall()
+    for state in rows:
+        print(state)
