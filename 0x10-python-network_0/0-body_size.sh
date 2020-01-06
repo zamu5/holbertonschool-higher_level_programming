36258ea920cb63747056bafd3d7dbb6ad4c6a2b4
Sergio Zamudio <szamu5@hotmail.com>
2020-01-06 14:07:26 -0500

    FIX: change the input by user input

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # print the size of the body response
-curl -sI http://2f3050f3cd04.38.hbtn-cod.io:5000 | awk '/Content-Length/ {print $2}'
+curl -sI "$1" | awk '/Content-Length/ {print $2}'
