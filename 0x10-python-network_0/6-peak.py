af06cd135d2e6cd1e14aa02f0166a3960317091a
Sergio Zamudio <szamu5@hotmail.com>
2020-01-07 17:16:25 -0500

    FIX: find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,15 +2,24 @@
 """
 finds a peak in a list of unsorted integers.
 """
-
-
 def find_peak(list_of_integers):
-    li = list_of_integers
-    if li == []:
+    """
+    """
+    if len(list_of_integers) == 0:
         return None
-    for i in range(len(li) - 1):
-        if i > 0 and i < len(li) - 1:
-            if li[i] > li[i - 1] and li[i] > li[i + 1]:
-                return li[i]
-            if li[i] == li[i - 1] and li[i] == li[i + 1] and li[i] == max(li):
-                return li[i]
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if list_of_integers[1] <= list_of_integers[0]:
+        return list_of_integers[0]
+    if list_of_integers[-1] >= list_of_integers[-2]:
+        return list_of_integers[-1]
+    mid = len(list_of_integers) // 2
+    if list_of_integers[mid] >= list_of_integers[mid - 1] \
+            and list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+
+    if list_of_integers[mid + 1] > list_of_integers[mid]:
+        return(find_peak(list_of_integers[mid + 1:len(list_of_integers)]))
+
+    if list_of_integers[mid - 1] > list_of_integers[mid]:
+        return(find_peak(list_of_integers[0:mid]))
