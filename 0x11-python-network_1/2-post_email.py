c928eab54934883207aab82a8e37b1cd905e1ffe
Sergio Zamudio <szamu5@hotmail.com>
2020-01-13 18:13:57 -0500

    FEATURE: sends a POST request to the passed URL with the email as a parameter New file: 2-post_email.py Command: data = urllib.parse.urlencode(values), data = data.encode('utf-8')

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# sends a POST request to the passed URL with the email as a parameter
+
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {'email' : sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
