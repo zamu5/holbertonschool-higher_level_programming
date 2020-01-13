47f1e7dcd3e906b16cc35272f520bc933dd1197f
Sergio Zamudio <szamu5@hotmail.com>
2020-01-13 14:19:08 -0500

    FEATURE: request the content of a web

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# fetches https://intranet.hbtn.io/status
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        the_page = response.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(the_page)))
+        print("\t - content: {}".format(the_page))
+        print("\t - utf8 content: {}".format(the_page.decode('utf8')))
