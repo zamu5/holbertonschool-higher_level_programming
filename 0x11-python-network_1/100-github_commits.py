bb2b735460cee68cf70cce72b5fec4aa97c5873d
Sergio Zamudio <szamu5@hotmail.com>
2020-01-13 23:47:21 -0500

    FIX: order of arguments

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,8 @@ import requests
 
 if __name__ == "__main__":
     try:
-        url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1],
-                                                                  sys.argv[2])
+        url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
+                                                                  sys.argv[1])
         req = requests.get(url)
         json_res = req.json()
         for commit in range(0, 10):
