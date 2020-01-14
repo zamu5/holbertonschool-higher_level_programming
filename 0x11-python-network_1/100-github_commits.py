47397874fb22d2f371469b53b13b88f2c8d47008
Sergio Zamudio <szamu5@hotmail.com>
2020-01-13 23:39:54 -0500

    FEATURE: list the last 10 commits from a repo New file:  100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# look for commits
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1],
+                                                              sys.argv[2])
+    req = requests.get(url)
+    json_res = req.json()
+    for commit in range(0, 10):
+        print("{}: {}".format(json_res[commit].get('sha'),
+                              json_res[commit].get('commit')
+                              .get('author').get('name')))
