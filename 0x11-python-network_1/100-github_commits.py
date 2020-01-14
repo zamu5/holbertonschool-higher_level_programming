c2435c15457d88e3ca6473b1e19801af0926f530
Sergio Zamudio <szamu5@hotmail.com>
2020-01-13 23:43:26 -0500

    FIX: make an except in case of fail

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,11 +5,14 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1],
-                                                              sys.argv[2])
-    req = requests.get(url)
-    json_res = req.json()
-    for commit in range(0, 10):
-        print("{}: {}".format(json_res[commit].get('sha'),
-                              json_res[commit].get('commit')
-                              .get('author').get('name')))
+    try:
+        url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1],
+                                                                  sys.argv[2])
+        req = requests.get(url)
+        json_res = req.json()
+        for commit in range(0, 10):
+            print("{}: {}".format(json_res[commit].get('sha'),
+                                  json_res[commit].get('commit')
+                                  .get('author').get('name')))
+    except:
+        None
