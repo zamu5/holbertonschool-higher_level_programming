c4d3b10eaa47adf46d66ccd76180b04cc2e0843d
Sergio Zamudio <szamu5@hotmail.com>
2020-02-04 18:04:28 -0500

    FIX: evaluate when the biggests is twice

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,14 @@
 if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
-  const ar = [];
-  let i;
+  const ar = []; let i;
   for (i = 2; process.argv[i]; i++) {
     ar.push(parseInt(process.argv[i]));
   }
-  console.log(ar.sort().reverse()[1]);
+  i = 0;
+  ar.sort().reverse();
+  while (ar[i] === ar[i + 1]) {
+    i++;
+  }
+  console.log(ar[i + 1]);
 }
