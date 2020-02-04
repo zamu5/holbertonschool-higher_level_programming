97479bb80425c25163a838da7b45676fbacf9bed
Sergio Zamudio <szamu5@hotmail.com>
2020-02-04 14:45:41 -0500

    FIX: parse number to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (process.argv.length === 2 || isNaN(Number(process.argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log(Number(process.argv[2]));
+  console.log('My number: ' + parseInt(Number(process.argv[2])));
 }
