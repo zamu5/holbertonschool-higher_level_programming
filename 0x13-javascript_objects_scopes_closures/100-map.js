b22a6557b3ee43106e736227cdb92b7f075c8d80
Sergio Zamudio <szamu5@hotmail.com>
2020-02-10 16:25:35 -0500

    FIX: change the import file name

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list1 = require('./100-main').list;
+const list1 = require('./100-data.js').list;
 const newlist = list1.map(function (value, index) {
   return value * index;
 });
