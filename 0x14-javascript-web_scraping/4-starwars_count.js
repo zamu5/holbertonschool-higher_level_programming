19825d27b0136e5715f1c9504d66b904a9f33dd9
Sergio Zamudio <szamu5@hotmail.com>
2020-02-11 13:44:50 -0500

    FIX: handle error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (_error, _response, body) {
-  let count = 0;
-  JSON.parse(body).results.forEach(element => {
-    element.characters.forEach(data => {
-      if (data === 'https://swapi.co/api/people/18/') {
-        count++;
-      }
-    });
-  })
-  ;
-  console.log(count);
+request(process.argv[2], function (err, _response, body) {
+  if (err === null) {
+    console.log(err);
+  } else {
+    let count = 0;
+    JSON.parse(body).results.forEach(element => {
+      element.characters.forEach(data => {
+        if (data === 'https://swapi.co/api/people/18/') {
+          count++;
+        }
+      });
+    })
+    ;
+    console.log(count);
+  }
 });
