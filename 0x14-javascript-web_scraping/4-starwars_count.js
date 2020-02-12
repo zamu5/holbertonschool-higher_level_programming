2a40414f0c729f13e84e015b8772bd30795c3501
Sergio Zamudio <szamu5@hotmail.com>
2020-02-11 21:57:29 -0500

    FIX: handle err

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,8 +2,6 @@
 const request = require('request');
 request(process.argv[2], function (err, _response, body) {
   if (err === null) {
-    console.log(err);
-  } else {
     let count = 0;
     JSON.parse(body).results.forEach(element => {
       element.characters.forEach(data => {
