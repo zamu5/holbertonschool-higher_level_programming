62dde80c0e95d54b87cb3d19027f38a16223bdfc
Sergio Zamudio <szamu5@hotmail.com>
2020-02-11 23:25:01 -0500

    FEATURE: print names in order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const newDict = {}
+request('http://swapi.co/api/films/' + process.argv[2], function (_error, _response, body) {
+  JSON.parse(body).characters.forEach(function (element, index, array) {
+    request(element, function (_err, _res, bod) {
+      saveData(index, JSON.parse(bod).name, array);
+    });
+  });
+});
+function saveData (i, name, array) {
+  newDict[i] = name;
+  if (Object.keys(newDict).length === array.length) {
+    let i;
+    for (i = 0; i < array.length; i++) {
+      console.log(newDict[i]);
+    }
+  }
+}
